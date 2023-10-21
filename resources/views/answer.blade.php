6c3a46961a5a61b1c10ecd831627f2ff2560d98d
hachim-soutra <hachimsoutra10@gmail.com>
2023-10-21 17:05:43 +0200

    xx

--- resources/views/answer.blade.php
+++ resources/views/answer.blade.php
@@ -77,15 +77,6 @@
                                         {{ $question->question->error }}
                                     </strong>
                                     <br>
-                                    @dd(
-                                        $question->question->options()->pluck('value')->toArray(),
-                                        array_values($answer->answers[$question->question->id]),
-                                        !Helper::compareArray(
-                                            $question->question->options()->pluck('value')->toArray(),
-                                            array_values($answer->answers[$question->question->id]),
-                                        ),
-                                        $question->question->error
-                                    )
                                 @endif
                             @else
                                 @foreach ($question->question->options as $option)
