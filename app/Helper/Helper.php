280bcf08e6eed00951e60ba83cc939788bc6af8f
hachim-soutra <hachimsoutra10@gmail.com>
2023-10-22 00:18:27 +0200

    wip

--- app/Helper/Helper.php
+++ app/Helper/Helper.php
@@ -6,8 +6,11 @@ class Helper
 {
     public function compareArray($array1, $array2)
     {
-        array_multisort($array1);
-        array_multisort($array2);
-        return serialize($array1) === serialize($array2);
+        foreach ($array1 as $arr) {
+            if (!in_array($arr, $array2)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
